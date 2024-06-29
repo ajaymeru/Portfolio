2f6abf3314f54035fa46d9505bfb952f8590a66d
ajaymeru <123156354+ajaymeru@users.noreply.github.com>
2024-06-29 17:18:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Experience from './Components/Experience/Experience';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Work from './Components/Work/Work';
+import Intro from './Components/Intro/Intro';
+import Navbar from './Components/Navbar/Navbar';
+import Services from './Components/Services/Services';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import { useContext } from "react";
+import { themeContext } from "./Context";
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}>
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Work />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
